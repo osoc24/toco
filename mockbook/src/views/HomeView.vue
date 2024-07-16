4d90b3bea9305f4a68ec6d289d0846afa02e5e27
Barra Victor <victor.barra@student.howest.be>
2024-07-16 16:57:17 +0200

    styling mockbook dashboard mock data

--- mockbook/src/views/HomeView.vue
+++ mockbook/src/views/HomeView.vue
@@ -1,25 +1,58 @@
 <template>
+  <Header pfp-src="https://thispersondoesnotexist.com" :name="userProfile.name"></Header>
   <main>
-    <button @click="logout">Logout</button>
-
-    <hr />
-    <Suspense>
-      <!-- <PodList /> -->
-
-      <template #fallback> Loading pods... </template>
-    </Suspense>
+    <div class="profile">
+      <p>{{ userProfile.description }}</p>
+      <p>{{ userProfile.mbox }}</p>
+    </div>
+    <Posts :posts="postsData" />
   </main>
 </template>
 
 <script setup lang="ts">
-// import PodList from "../components/PodList.vue";
-import router from "@/router";
-import { store } from "@/store";
+import { onMounted, ref } from 'vue';
+import Header from '@/components/home/Header.vue';
+import Posts from '@/components/home/Posts.vue';
+
+import { fetchUserProfile, fetchPosts, test } from '@/lib/solid';
+import router from '@/router';
+import { store } from '@/store';
+import type { Session } from '@inrupt/solid-client-authn-browser';
 
 const session = store.session;
+const userProfile = ref({
+  name: 'Default Name',
+  mbox: '',
+  img: '',
+  description: ''
+});
+const postsData = ref<{name: string, image: string, text: string, video: string}[]>([]);
 
-const logout = async () => {
-  session.logout();
-  router.push("/");
-};
+const podUrl = 'https://css12.onto-deside.ilabt.imec.be';
+
+// const fetchedUserProfile = await test(session as Session, podUrl);
+// console.log("fetchedUserProfile", fetchedUserProfile);
+
+// userProfile.value = {
+//   name: fetchedUserProfile.name ?? '',
+//   mbox: fetchedUserProfile.mbox ?? '',
+//   img: fetchedUserProfile.img ?? '',
+//   description: fetchedUserProfile.description ?? ''
+// };
+// postsData.value = await fetchPosts(session, podUrl);
+
+const postDataItems = [
+  { name: userProfile.value.name, image: "", text: "Hi, team Inc!", video: "" },
+  { name: userProfile.value.name, image: "https://via.placeholder.com/150", text: "New profile pic", video: "" }
+]
+
+
+postsData.value = postDataItems;
 </script>
+
+<style scoped>
+main {
+  padding: 4rem;
+}
+
+</style>
